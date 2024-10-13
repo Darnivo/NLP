9e2da6c59c06d115c42a35325eba8fa4b7214064
Darnivo <164860862+Darnivo@users.noreply.github.com>
2024-10-13 23:13:40 +0700

    Add files via upload

---
+++ S3 Sep 24/test.py
@@ -0,0 +1,22 @@
+import numpy as np
+print("x")
+
+
+## SESSION 3
+
+#POS (Part of speech) tagging > determine what the work/token is what part of the speech
+#e.g "I like your watch" >> (I > pronoun) (like > verb) (your > verb) (watch > noun)
+#e.g2 "I watch movies"  >> (I > still pronoun) (watch > verb NOT noun) (movies > noun)
+
+#use case   > resolve word ambiguity
+#uc2        > non literal translation 
+#uc3        > autocomplete
+#uc4        > text to speech pronounciation (eg homographs like lead)
+
+
+
+# Freq. distribution 
+#use case   > sentiment analysis
+
+# WordNet   >> mapping synonyms and antonyms of a word
+#use case  
---
+++ readme.txt
@@ -0,0 +1,7 @@
+activate w/:
+.venv\Scripts\activate
+
+
+read w/:
+import nltk
+nltk.data.find('tokenizers/punkt.zip')
